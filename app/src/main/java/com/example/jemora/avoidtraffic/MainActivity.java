683137c6e06268c3a3b5ea07a709b91110559d97
vtang4 <vtang1220@gmail.com>
2018-12-04 17:39:25 -0800

    added graphView dependency, NetworkUtils

--- app/src/main/java/com/example/jemora/avoidtraffic/MainActivity.java
+++ app/src/main/java/com/example/jemora/avoidtraffic/MainActivity.java
@@ -2,8 +2,22 @@ package com.example.jemora.avoidtraffic;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.jjoe64.graphview.GraphView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText destination;
+    private EditText departTime;
+    private TextView departTimeTextView;
+    private TextView destinationTextView;
+    private Button saveButton;
+     private GraphView graphView;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -11,6 +25,17 @@ public class MainActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_main);
 
+        departTimeTextView = findViewById(R.id.depart_time);
+        departTime = findViewById(R.id.depart_time_edit_text);
+        destination = findViewById(R.id.destination_edit_text);
+        destinationTextView = findViewById(R.id.destination);
+        graphView = findViewById(R.id.graph);
+        saveButton = findViewById(R.id.save);
+
 
     }
+
+
+
+
 }
